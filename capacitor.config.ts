ce08e39120ea2977393ec0889633f788a918aee3
mdriaz <riazmd582@gmail.com>
2022-04-14 15:36:12 +0600

    android setup is done

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'bmi-calc',
+  appId: 'ml.mdriaz.bmiCalc',
+  appName: 'BMI Calculator',
   webDir: 'build',
   bundledWebRuntime: false
 };
